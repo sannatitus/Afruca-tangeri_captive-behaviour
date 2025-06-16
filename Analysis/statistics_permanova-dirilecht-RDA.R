3fa2f576dcabdc6809b48cf4033edb71bac735fb
Sanna Eriksson Titus <sannatitus@yahoo.com>
2025-06-16 17:17:47 +0100

    Update statistics_permanova-dirilecht-RDA.R

--- Analysis/statistics_permanova-dirilecht-RDA.R
+++ Analysis/statistics_permanova-dirilecht-RDA.R
@@ -1,4 +1,5 @@
-# R script to run univariate and multivariate PERMANOVAs, Dirichlet regression, and RDA. Composed by Sanna Titus, May 2025. 
+# R script to run univariate and multivariate PERMANOVAs, Dirichlet regression, and RDA for instantaneous observations of Afruca tangeri in two crabitats (110 tank & tub) at the Sainsbury Wellcome Centre for Neural Circuits and Animal Behaviour throughout 2022-2023. 
+## Composed by Sanna Titus | sannatitus@gmail.com | 16 June 2025
 # install.packages("readxl")
 # install.packages("dplyr")
 # install.packages("vegan")
